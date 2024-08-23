a436b66baf25d884e39e3d3f700332524129fb46
milansir <rwn3developer11@gmail.com>
2024-08-23 16:50:31 +0530

    dynamic form component

--- dynamic-form/src/App.jsx
+++ dynamic-form/src/App.jsx
@@ -2,17 +2,28 @@ import { useState } from "react"
 
 function App() {
 
-  const [input, setInput] = useState([
-    {id : '', name: '', phone: '' },
-  ])
+  const [input, setInput] = useState([{ id: Math.random(), name: '', phone: '' }]);
+
+  const [disable, setDisable] = useState([]);
 
   const addMore = () => {
-    let newfield = { id : Math.floor(Math.random()*10000) , name: '', phone: '' };
+    let newfield = { id: Math.random(), name: '', phone: '' };
     setInput([...input, newfield]);
   }
+
   const removeBtn = (id) => {
-    let deleteData = input.filter(val => val.id != id);
+    let deleteData = input.filter(val => val.id !== id);
     setInput(deleteData);
+  }
+
+  const doneBtn = (id) => {
+    if (!disable.includes(id)) {
+      setDisable([...disable, id]);
+    }
+  }
+
+  const editBtn = (id) => {
+    setDisable(disable.filter(disableId => disableId !== id));
   }
 
   return (
@@ -20,30 +31,43 @@ function App() {
       <h2>Dynamic Form</h2>
 
       {
-        input.map((val,index) => {
-          return (
-            <div key={val.id}>
-              <div>
-                Name : <input type="text" /> &nbsp;
-                Phone : <input type="no" /> &nbsp;
-                {
-                    index != 0 ? (
-                      <button onClick={ () => removeBtn(val.id) }>remove</button>
-                    ) : (
-                      ""
-                    )
-                }
-                
-              </div>
-              <br></br>
+        input.map((val) => (
+          <div key={val.id}>
+            <div>
+              {
+                disable.includes(val.id) ? (
+                  <>
+                    Name: <input type="text" value={val.name} disabled /> &nbsp;
+                    Phone: <input type="text" value={val.phone} disabled /> &nbsp;
+                    <button onClick={() => editBtn(val.id)}>Edit</button>
+                  </>
+                ) : (
+                  <>
+                    Name: <input type="text" value={val.name} onChange={(e) => {
+                      const updatedInputs = input.map((item) =>
+                        item.id === val.id ? { ...item, name: e.target.value } : item
+                      );
+                      setInput(updatedInputs);
+                    }} /> &nbsp;
+                    Phone: <input type="text" value={val.phone} onChange={(e) => {
+                      const updatedInputs = input.map((item) =>
+                        item.id === val.id ? { ...item, phone: e.target.value } : item
+                      );
+                      setInput(updatedInputs);
+                    }} /> &nbsp;
+                    <button onClick={() => doneBtn(val.id)}>Done</button>
+                    <button onClick={() => removeBtn(val.id)}>Remove</button>
+                  </>
+                )
+              }
             </div>
-          )
-        })
+            <br />
+          </div>
+        ))
       }
-      <button onClick={() => addMore()}>Add</button>
-
+      <button onClick={addMore}>Add</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
