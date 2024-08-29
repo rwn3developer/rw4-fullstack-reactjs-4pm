1e3b65d450be30ddc39d74ac0591750ed7bf4ba6
milansir <rwn3developer11@gmail.com>
2024-08-29 16:59:28 +0530

    bootstrap install react bootstrap components

---
+++ bootstrap components/src/App.jsx
@@ -0,0 +1,20 @@
+import Navbarexample from "./Navbar"
+import BasicExample from "./Sliderexample"
+
+
+
+
+
+
+
+function App() {
+
+  return (
+    <>
+      <Navbarexample/>
+      <BasicExample/>
+    </>
+  )
+}
+
+export default App
