1bec560d6d6c89612f4655d1ce186bec60a343d2
rwn3developer <rwn3developer11@gmail.com>
2024-10-10 15:56:28 +0530

    firebase-realtimedb-crud done

--- firebase-realtime-crud/src/App.jsx
+++ firebase-realtime-crud/src/App.jsx
@@ -1,33 +1,18 @@
-import { useState } from "react";
-import { getDatabase, ref, set } from "firebase/database";
-import { app } from "../firebase";
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Add from "./pages/Add"
+import View from "./pages/View"
+import Edit from "./pages/Edit"
+
 
 function App() {
-  const [name, setName] = useState("")
-  const [phone, setPhone] = useState("")
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const db = getDatabase(app)
-    let id = Math.floor(Math.random() * 100000);
-    set(ref(db, `users/${id}`), {
-      name: name,
-      phone: phone
-    })
-    alert("record add");
-    setName("");
-    setPhone("");
-  }
   return (
-    <div align="center">
-      <h2>Add Record</h2>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Name:</label><br />
-        <input type="text" onChange={(e) => setName(e.target.value)} value={name} /><br /><br />
-        <label htmlFor="phone">Phone Number:</label><br />
-        <input type="number" onChange={(e) => setPhone(e.target.value)} value={phone} /><br /><br />
-        <input type="submit" defaultValue="Submit" />
-      </form>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<View />} />
+        <Route path="/add" element={<Add />} />
+        <Route path="/edit" element={<Edit />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 export default App
